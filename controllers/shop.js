099a3a0624fb9cd93b6f90afae672d230cf5854c
Nikhilpal1995 <nikhilpal1720@gmail.com>
2023-10-03 20:34:07 +0530

    Making SQL easy with Sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -69,11 +69,10 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
-    .then(() => {
-      res.redirect('/cart'); // Redirect to the cart page after successful deletion.
-    })
-    .catch(err => console.log(err));
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  });
 };
 
 exports.getOrders = (req, res, next) => {
